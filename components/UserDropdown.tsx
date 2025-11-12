13c8fdf8380f275f16491183e9fc5e859a7f424f
chittaranjan27 <kchittaranjandora@gmail.com>
2025-11-12 19:13:04 +0530

    initialized stocks search and details

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -15,7 +15,7 @@ import NavItems from "./NavItems"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {signOut} from "@/lib/actions/auth.actions";
 
-const UserDropdown = ({user}: {user: User}) => {
+const UserDropdown = ({user, initialStocks}: {user: User, initialStocks:StockWithWatchlistStatus[]}) => {
     const router = useRouter();
 
     const handelSignOut = async () => {
@@ -64,7 +64,7 @@ const UserDropdown = ({user}: {user: User}) => {
                 </DropdownMenuItem>
                 <DropdownMenuSeparator className="hidden sm:block bg-gray-600"/>
                 <nav className="sm:hidden">
-                    <NavItems/>
+                    <NavItems initialStocks={initialStocks}/>
                 </nav>
             </DropdownMenuContent>
         </DropdownMenu>
--- components/forms/SelectField.tsx
+++ components/forms/SelectField.tsx
@@ -12,7 +12,9 @@ import {
 const SelectField = ({ name, label, placeholder, options, control, error, required = false }: SelectFieldProps) => {
   return (
     <div className='space-y-2'>
-      <Label htmlFor={name} className="form-label">{label}</Label>
+      <Label htmlFor={name} className="form-label"> 
+        {label}
+      </Label>
 
       <Controller
         name={name}
